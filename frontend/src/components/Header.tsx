8d3f2c1ef0157fe6350eac63ceeadfb5c227c6f5
Abdullah Yakub <abdullah.yakub@hotmail.com>
2024-02-23 05:00:03 +1100

    stretch task 1 answer

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,11 +1,23 @@
+import { useState } from "react"
+
 const Header = () => {
+  const blueColor = "md:text-6xl text-4xl font-extrabold text-[#1479f2]"
+
+  const [titleColor, setTitleColor] = useState(blueColor)
+
+  const redColor = "md:text-6xl text-4xl font-extrabold text-red-500"
+
+  const handleColorChange = () => {
+    setTitleColor(prevColor => prevColor === blueColor ? redColor : blueColor)
+  }
   return (
     <header className="w-4/5">
 
       <p>DevSoc presents</p>
-      <h1 className="md:text-6xl text-4xl font-extrabold text-[#1479f2]">unilectives</h1>
+      <h1 className={titleColor}
+        onClick={handleColorChange}>unilectives</h1>
       <p className="font-extrabold">Your one-stop shop for UNSW course and elective reviews.</p>
-    </header>
+    </header >
   )
 }
 
